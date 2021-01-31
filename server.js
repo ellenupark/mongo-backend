314b34a36bf88afb9cc5c3ee02d36a68fe62d9fd
ellenupark <ellenupark@gmail.com>
2021-01-31 18:37:16 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,37 @@
+// establish an express app
+const express = require('express')
+const app = express()
+
+// allow requests from outside resources like postman, or your frontend if you choose to build that out
+const cors = require('cors')
+app.use(cors())
+
+// app will serve and receive data in a JSON format
+app.use(express.json())
+
+// the messenger between our app and our database
+const mongoose = require('mongoose')
+
+// allow us to hide our connection secret in the process.env object
+require('dotenv').config()
+
+// establish connection & give yourself a message so you know when its complete
+const source = process.env.ATLAS_CONNECTION
+
+mongoose.connect(source, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log("DB connected.");
+})
+
+const userRoutes = require('./controllers/user.controller')
+app.use('/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
+    console.log(`Successfully served on port: ${PORT}.`);
+})
